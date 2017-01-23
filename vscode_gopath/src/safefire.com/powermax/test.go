842b0a3839f630c87e5039215d621889470e51f5
zloathleo <zloathleo@gmail.com>
2017-01-23 09:30:28 +0800

    vscode first time

---
+++ vscode_gopath/src/safefire.com/powermax/test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+
+	"github.com/ugorji/go/codec"
+)
+
+type value struct {
+	Tm     uint64
+	Value  int
+	Status int
+}
+
+func main() {
+	fmt.Println("Hello, word")
+	var testValue = &value{1000, 99, 998}
+
+	var w = bytes.NewBuffer([]byte{})
+	var bw = bufio.NewWriter(w)
+	var h codec.Handle = new(codec.MsgpackHandle)
+	var enc = codec.NewEncoder(bw, h)
+
+	fmt.Println(testValue.Status)
+	var err = enc.Encode(testValue) //any of v1 ... v8
+	bw.Flush()
+
+	var length = len(w.Bytes())
+	// var str = strconv.Itoa(length)
+	fmt.Println(length)
+	fmt.Println(err)
+
+	var value2 = new(value)
+	var dec = codec.NewDecoderBytes(w.Bytes(), h)
+	var err2 = dec.Decode(&value2)
+	fmt.Println(err2)
+
+	fmt.Println(value2.Status)
+
+}
